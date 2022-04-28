30d958a6f2d71d3499663919964e117a8e137a05
Thomas LE CLERE <tleclere@jahia.com>
2022-04-28 15:33:23 +0200

    Custom validator : check HTML in CKEditor contribution

---
+++ src/main/java/org/foo/modules/jahia/validators/CustomJCRNodeValidatorDefinition.java
@@ -0,0 +1,18 @@
+package org.foo.modules.jahia.validators;
+
+import org.jahia.osgi.BundleUtils;
+import org.jahia.services.content.decorator.validation.JCRNodeValidatorDefinition;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Collections;
+
+@Component(service = JCRNodeValidatorDefinition.class, immediate = true)
+public class CustomJCRNodeValidatorDefinition extends JCRNodeValidatorDefinition {
+    @Activate
+    private void onActivate(BundleContext bundleContext) {
+        setJahiaModule(BundleUtils.getModule(bundleContext.getBundle()));
+        setValidators(Collections.singletonMap("foont:text", FoontTextValidator.class.getName()));
+    }
+}
---
+++ src/main/java/org/foo/modules/jahia/validators/HtmlValid.java
@@ -0,0 +1,17 @@
+package org.foo.modules.jahia.validators;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = TextValidator.class)
+@Documented
+public @interface HtmlValid {
+    Class<?>[] groups() default {};
+
+    String message() default "{javax.validation.constraints.htmlinvalid}";
+
+    Class<? extends Payload>[] payload() default {};
+}
