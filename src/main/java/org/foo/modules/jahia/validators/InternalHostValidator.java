802c886036306953910b5c5522756586d33dc18b
Thomas LE CLERE <tleclere@jahia.com>
2023-02-17 15:39:58 +0100

    FIX host validator

--- src/main/java/org/foo/modules/jahia/validators/InternalHostValidator.java
+++ src/main/java/org/foo/modules/jahia/validators/InternalHostValidator.java
@@ -3,6 +3,9 @@ package org.foo.modules.jahia.validators;
 import org.apache.commons.lang.StringUtils;
 import org.jahia.osgi.BundleUtils;
 import org.jahia.services.content.decorator.JCRSiteNode;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+import org.jahia.services.render.filter.HtmlTagAttributeTraverser;
 import org.jahia.services.sites.JahiaSitesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,11 +16,23 @@ import javax.validation.ConstraintValidatorContext;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class InternalHostValidator implements ConstraintValidator<InternalHost, FoomixTextValidator> {
     private static final Logger logger = LoggerFactory.getLogger(InternalHostValidator.class);
 
     private String errorMessage;
+    private final HtmlTagAttributeTraverser htmlTagAttributeTraverser;
+
+    public InternalHostValidator() {
+        Map<String, Set<String>> attributesToVisit = new HashMap<>();
+        attributesToVisit.put("a", Collections.singleton("href"));
+        attributesToVisit.put("img", Collections.singleton("src"));
+        htmlTagAttributeTraverser = new HtmlTagAttributeTraverser(attributesToVisit);
+    }
 
     @Override
     public void initialize(InternalHost internalHostValidation) {
@@ -51,12 +66,22 @@ public class InternalHostValidator implements ConstraintValidator<InternalHost, 
             if (value == null) {
                 return false;
             }
-            URI uri = new URI(value);
-            return BundleUtils.getOsgiService(JahiaSitesService.class, null).getSitesNodeList().stream()
-                    .map(JCRSiteNode::getAllServerNames)
-                    .flatMap(Collection::stream)
-                    .noneMatch(serverName -> StringUtils.equals(uri.getHost(), serverName));
-        } catch (URISyntaxException | RepositoryException e) {
+
+            return htmlTagAttributeTraverser.traverse(value, (String v, RenderContext context, String tagName, String attrName, Resource resource) -> {
+                try {
+                    URI uri = new URI(v);
+                    if (BundleUtils.getOsgiService(JahiaSitesService.class, null).getSitesNodeList().stream()
+                            .map(JCRSiteNode::getAllServerNames)
+                            .flatMap(Collection::stream)
+                            .noneMatch(serverName -> StringUtils.equals(uri.getHost(), serverName))) {
+                        return v;
+                    }
+                } catch (URISyntaxException | RepositoryException e) {
+                    logger.error("", e);
+                }
+                return null;
+            }) != null;
+        } catch (Exception e) {
             logger.error("", e);
             return false;
         }
