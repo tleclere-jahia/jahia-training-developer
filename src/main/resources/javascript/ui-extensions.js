af0cbce94447854a0a6beebabd6864ff4d809452
Thomas LE CLERE <tleclere@jahia.com>
2023-12-18 19:00:32 +0100

    Portlet picker

---
+++ src/main/resources/javascript/ui-extensions.js
@@ -0,0 +1,27 @@
+window.jahia.i18n.loadNamespaces('jahia-training-developer');
+
+window.jahia.uiExtender.registry.add('callback', 'jahia-training-developer-ui-extensions', {
+    targets: ['jahiaApp-init:6'],
+    callback: function () {
+        window.jahia.uiExtender.registry.add('adminRoute', 'portletsite', {
+            targets: ['jcontent:40'],
+            label: 'jahia-training-developer:label.portletmanager.title',
+            isSelectable: true,
+            render: ({match}) => window.jahia.ui.getIframeRenderer(`${window.contextJsParameters.contextPath}/engines/manager.jsp?conf=portletmanager-anthracite&lang=${match.params.lang}&site=${window.contextJsParameters.siteUuid}`)
+        })
+    }
+});
+
+window.jahia.uiExtender.registry.add('pickerConfiguration', 'portlet', {
+    selectableTypesTable: ['jnt:portlet'],
+    accordions: ['picker-content-folders'],
+    accordionItem: {
+        "picker-content-folders": {
+            rootPath: '/sites/{site}/portlets',
+            treeConfig: {
+                hideRoot: false
+            },
+            label: 'jahia-training-developer:label.portlets.title'
+        }
+    }
+});
