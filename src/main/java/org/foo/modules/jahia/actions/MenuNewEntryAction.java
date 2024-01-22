94df53cbfe7cb520ff1430ef1c0fe034376ef03c
Thomas LE CLERE <tleclere@jahia.com>
2024-01-22 16:41:40 +0100

    Remove spring configuration

--- src/main/java/org/foo/modules/jahia/actions/MenuNewEntryAction.java
+++ src/main/java/org/foo/modules/jahia/actions/MenuNewEntryAction.java
@@ -17,6 +17,30 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * GWT menu entry extension
+ * You need a Spring Configuration :
+ * src/main/resources/META-INF/spring/jahia-training-developer.xml
+ *     <bean class="org.jahia.services.uicomponents.bean.toolbar.Item">
+ *         <property name="actionItem">
+ *             <bean class="org.jahia.ajax.gwt.client.widget.toolbar.action.ExecuteActionItem">
+ *                 <property name="action" value="menuNewEntry"/>
+ *                 <property name="allowedNodeTypes" value="jnt:page"/>
+ *             </bean>
+ *         </property>
+ *         <property name="requiredPermission" value="menuCreatePageArticle"/>
+ *         <property name="titleKey" value="label.menuNewEntry@resources.jahia-training-developer"/>
+ *         <property name="parent" value="editmode.tabs[0].treeContextMenu"/>
+ *         <property name="positionAfter" value="Toolbar.Item.EditContent"/>
+ *         <property name="hideWhenDisabled" value="true"/>
+ *         <property name="requiredModule" value="jahia-training-developer"/>
+ *     </bean>
+ *
+ * pom.xml
+ *     <properties>
+ *         <require-capability>osgi.extender;filter:="(osgi.extender=org.jahia.bundles.blueprint.extender.config)"</require-capability>
+ *     </properties>
+ */
 @Component(service = Action.class)
 public class MenuNewEntryAction extends Action {
     private static final Logger logger = LoggerFactory.getLogger(MenuNewEntryAction.class);
