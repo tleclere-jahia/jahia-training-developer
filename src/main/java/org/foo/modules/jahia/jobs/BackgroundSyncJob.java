0852a2f15c4fbbab4cacbdab40a1d9954f326f3c
Thomas LE CLERE <tleclere@jahia.com>
2024-09-27 12:29:41 +0200

    Background job

---
+++ src/main/java/org/foo/modules/jahia/jobs/BackgroundSyncJob.java
@@ -0,0 +1,49 @@
+package org.foo.modules.jahia.jobs;
+
+import org.jahia.api.settings.SettingsBean;
+import org.jahia.osgi.BundleUtils;
+import org.jahia.services.scheduler.BackgroundJob;
+import org.jahia.services.scheduler.SchedulerService;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.SimpleTrigger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+public class BackgroundSyncJob extends BackgroundJob {
+    private static final Logger logger = LoggerFactory.getLogger(BackgroundSyncJob.class);
+
+    private static final int REPEAT_INTERVAL = 5 * 60 * 1000;
+
+    @Reference
+    private SchedulerService schedulerService;
+    @Reference
+    private SettingsBean settingsBean;
+    private JobDetail jobDetail;
+
+    @Activate
+    public void start() throws Exception {
+        jobDetail = BackgroundJob.createJahiaJob("Simple background job made declared with OSGi", BackgroundSyncJob.class);
+        if (schedulerService.getAllJobs(jobDetail.getGroup()).isEmpty() && settingsBean.isProcessingServer()) {
+            schedulerService.getScheduler().scheduleJob(jobDetail, new SimpleTrigger(BackgroundSyncJob.class.getName() + "_trigger", jobDetail.getGroup(), SimpleTrigger.REPEAT_INDEFINITELY, REPEAT_INTERVAL));
+        }
+    }
+
+    @Deactivate
+    public void stop() throws Exception {
+        if (!schedulerService.getAllJobs(jobDetail.getGroup()).isEmpty() && settingsBean.isProcessingServer()) {
+            schedulerService.getScheduler().deleteJob(jobDetail.getName(), jobDetail.getGroup());
+        }
+    }
+
+    @Override
+    public void executeJahiaJob(JobExecutionContext jobExecutionContext) {
+        logger.info("Trigger job!");
+        BundleUtils.getOsgiService(BackgroundSyncService.class, null).executeJahiaJob();
+    }
+}
---
+++ src/main/java/org/foo/modules/jahia/jobs/BackgroundSyncService.java
@@ -0,0 +1,54 @@
+package org.foo.modules.jahia.jobs;
+
+import org.jahia.api.Constants;
+import org.jahia.api.content.JCRTemplate;
+import org.jahia.api.usermanager.JahiaUserManagerService;
+import org.jahia.services.content.JCRContentUtils;
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.JCRPublicationService;
+import org.jahia.services.content.JCRSessionFactory;
+import org.jahia.services.usermanager.JahiaUser;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jcr.RepositoryException;
+import java.util.Locale;
+import java.util.UUID;
+
+@Component(service = BackgroundSyncService.class)
+public class BackgroundSyncService {
+    private static final Logger logger = LoggerFactory.getLogger(BackgroundSyncService.class);
+    private static final String ROOT_NODE = "/sites/systemsite/contents";
+
+    @Reference
+    private JCRPublicationService jcrPublicationService;
+    @Reference
+    private JCRTemplate jcrTemplate;
+    @Reference
+    private JahiaUserManagerService jahiaUserManagerService;
+    @Reference
+    private JCRSessionFactory jcrSessionFactory;
+
+    public void executeJahiaJob() {
+        logger.info("Create and publish node");
+        JahiaUser savedUser = jcrSessionFactory.getCurrentUser();
+        jcrSessionFactory.setCurrentUser(jahiaUserManagerService.lookupRootUser().getJahiaUser());
+        try {
+            jcrTemplate.doExecuteWithSystemSessionAsUser(null, Constants.EDIT_WORKSPACE, Locale.FRENCH, session -> {
+                JCRNodeWrapper rootNode = session.getNode(ROOT_NODE);
+                JCRNodeWrapper textNode = rootNode.addNode(JCRContentUtils.findAvailableNodeName(rootNode, "text"), "jnt:text");
+                textNode.setProperty("text", UUID.randomUUID().toString());
+                textNode.saveSession();
+
+                jcrPublicationService.publishByMainId(textNode.getIdentifier());
+                return null;
+            });
+        } catch (RepositoryException e) {
+            logger.error("", e);
+        } finally {
+            jcrSessionFactory.setCurrentUser(savedUser);
+        }
+    }
+}
