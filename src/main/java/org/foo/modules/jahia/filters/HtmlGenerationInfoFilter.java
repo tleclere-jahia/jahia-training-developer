e8e5f22ca671e6b18bb0a0b34b7671501da6a5e8
Thomas LE CLERE <tleclere@jahia.com>
2024-11-26 16:34:36 +0100

    Render filter restricted to current module

--- src/main/java/org/foo/modules/jahia/filters/HtmlGenerationInfoFilter.java
+++ src/main/java/org/foo/modules/jahia/filters/HtmlGenerationInfoFilter.java
@@ -1,11 +1,17 @@
 package org.foo.modules.jahia.filters;
 
-import net.htmlparser.jericho.*;
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.EndTag;
+import net.htmlparser.jericho.HTMLElementName;
+import net.htmlparser.jericho.OutputDocument;
+import net.htmlparser.jericho.Source;
 import org.jahia.services.render.RenderContext;
 import org.jahia.services.render.Resource;
 import org.jahia.services.render.filter.AbstractFilter;
 import org.jahia.services.render.filter.RenderChain;
 import org.jahia.services.render.filter.RenderFilter;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,8 +26,12 @@ public class HtmlGenerationInfoFilter extends AbstractFilter {
 
     public HtmlGenerationInfoFilter() {
         setApplyOnConfigurations(Resource.CONFIGURATION_PAGE);
-        setApplyOnModes("preview,live");
         setPriority(10.1f);
+    }
+
+    @Activate
+    private void onActivate(BundleContext bundleContext) {
+        addCondition((renderContext, resource) -> renderContext.getSite().getInstalledModules().contains(bundleContext.getBundle().getSymbolicName()));
     }
 
     @Override
@@ -35,16 +45,16 @@ public class HtmlGenerationInfoFilter extends AbstractFilter {
     public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) {
         long startTime = (long) renderContext.getRequest().getAttribute(ATTRIBUTE_NAME);
         double timeEllapsed = (System.currentTimeMillis() - startTime) / 1000f;
-        logger.info("Time ellapsed: {}", String.format("%.2f", timeEllapsed));
+        logger.info("Time ellapsed: {}s", String.format("%.2f", timeEllapsed));
 
         Source source = new Source(previousOut);
         OutputDocument outputDocument = new OutputDocument(source);
         List<Element> bodyElementList = source.getAllElements(HTMLElementName.BODY);
-        if (bodyElementList.size() > 0) {
+        if (!bodyElementList.isEmpty()) {
             Element bodyElement = bodyElementList.get(bodyElementList.size() - 1);
             EndTag bodyEndTag = bodyElement.getEndTag();
             outputDocument.replace(bodyEndTag.getBegin(), bodyEndTag.getBegin() + 1,
-                    "<pre>" + "Time ellapsed: " + String.format("%.2f", timeEllapsed) + "</pre><");
+                    "<!-- Time ellapsed: " + String.format("%.2f", timeEllapsed) + "s --><");
         }
 
         return outputDocument.toString();
