082f29fbe95ac082590645c84ce13c3ba57e6069
Thomas LE CLERE <tleclere@jahia.com>
2025-03-04 18:04:31 +0100

    Review graphl extensions

--- src/main/java/org/foo/modules/jahia/graphql/CustomExtensionsRegister.java
+++ src/main/java/org/foo/modules/jahia/graphql/CustomExtensionsRegister.java
@@ -3,13 +3,6 @@ package org.foo.modules.jahia.graphql;
 import org.jahia.modules.graphql.provider.dxm.DXGraphQLExtensionsProvider;
 import org.osgi.service.component.annotations.Component;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 @Component(service = DXGraphQLExtensionsProvider.class)
 public class CustomExtensionsRegister implements DXGraphQLExtensionsProvider {
-    @Override
-    public Collection<Class<?>> getExtensions() {
-        return Arrays.asList(EmployeeGraphQLExtension.class, ResourceBundleExtension.class, GqlNodeExtension.class, CategorySubscriptionExtension.class);
-    }
 }
